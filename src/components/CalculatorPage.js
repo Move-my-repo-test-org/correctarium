aac73a19d616141eba2cfe95a402a85197dfe64d
NastiaKoval <anasteziyam@gmail.com>
2020-09-22 14:54:58 +0300

    feat: make a price calc func

--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -10,14 +10,23 @@ class CalculatorPage extends React.Component {
       email: '',
       name: '',
       text: '',
+      lettersCount: '',
       language: '',
       comment: '',
-      total: "0,00",
+      price: "0,00",
     };
   }
 
   onSubmit = (event) => {
     console.log(this.state);
+    this.setState({
+      email: '',
+      name: '',
+      text: '',
+      language: '',
+      comment: '',
+      price: "0,00",
+    })
     event.preventDefault();
   }
 
@@ -30,13 +39,22 @@ class CalculatorPage extends React.Component {
   }
 
   onChangeText = (e) => {
-    this.setState({text: e.target.value});
+    this.setState({text: e.target.value, lettersCount: e.target.value.length});
+    if (this.state.language) {
+      this.changePrice(e.target.value.length, this.state.language);
+    }
   }
 
   onChooseLanguage = (e) => {
     this.setState({language: e.target.value});
+    this.changePrice(this.state.text.length, e.target.value);
   }
 
+  changePrice = (lettersCount, language) => {
+    const letterCost = language === "eng" ? 0.12 : 0.05;
+    const currentPrice = lettersCount < 1000 && lettersCount !== 0 ? 1000 * letterCost : lettersCount * letterCost;
+    this.setState({price: currentPrice})
+  }
   onChangeComment = (e) => {
     this.setState({comment: e.target.value});
   }
@@ -51,8 +69,8 @@ class CalculatorPage extends React.Component {
         <form id="text-form" onSubmit={this.onSubmit}>
           <input type="text" name="email" value={this.state.email} placeholder="Ваша ел. пошта" required={true} onChange={this.onChangeEmail} />
           <input type="text" name="name" value={this.state.name} placeholder="Ваше ім'я" onChange={this.onChangeName} />
-          <textarea type="text" name="text" value={this.state.text} placeholder="Уведіть текст"
-              onChange={this.onChangeText}/>
+          <textarea className="textarea" type="text" name="text" value={this.state.text} placeholder="Уведіть текст" onChange={this.onChangeText} />
+          <p className="letters-count">{this.state.lettersCount ? this.state.lettersCount : ""}</p>
           <p>МОВА</p>
           <label>
             <input type="radio" name="language" value="ukr"  onChange={this.onChooseLanguage} />
@@ -70,7 +88,7 @@ class CalculatorPage extends React.Component {
         </form>
       </div>
       <div className="sum-btn-block">
-        <p>{this.state.total} грн</p>
+        <p>{this.state.price} грн</p>
         <button type="submit" form="text-form" >ЗАМОВИТИ</button>
       </div>
     </div>
